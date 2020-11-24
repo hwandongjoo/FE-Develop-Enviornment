61a0daf87fab2f23af38db47f0445dfc41936f02
hwandongjoo <32835661+hwandongjoo@users.noreply.github.com>
2020-11-24 16:10:39 +0900

    201123

--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -1,5 +1,9 @@
 const path = require("path");
-
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   mode: "development",
   entry: {
@@ -11,13 +15,18 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        use: [path.resolve("./my-webpack-loader.js")],
-      },
+      // {
+      //   test: /\.js$/,
+      //   use: [path.resolve("./my-webpack-loader.js")],
+      // },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
+          "css-loader",
+        ],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
@@ -31,4 +40,39 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")}
+        Author: ${childProcess.execSync("git config user.name")}
+      `,
+    }),
+    new webpack.DefinePlugin({
+      TWO: "1+1",
+      TWOTEXT: JSON.stringify(`1+1`),
+      "api.domain": JSON.stringify("http://dev.api.domain.com"),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "테스트",
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          new MiniCssExtractPlugin({
+            filename: "[name].css",
+          }),
+        ]
+      : []),
+  ],
 };
